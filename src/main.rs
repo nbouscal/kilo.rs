9b0b7be6bbd6e8f9d40fd839bdd234632f1abbd0
Nathan Bouscal <nbouscal@gmail.com>
2017-04-06 22:43:54 -0700

    Add rudimentary error handling

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,30 @@ static mut ORIG_TERMIOS: Option<libc::termios> = None;
 extern "C" fn disable_raw_mode() {
     unsafe {
         let mut termios = ORIG_TERMIOS.unwrap();
-        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+        let errno = libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+        if errno == -1 { panic!("tcsetattr") }
     }
 }
 
 fn enable_raw_mode() {
     unsafe {
         let mut termios: libc::termios = std::mem::zeroed();
-        libc::tcgetattr(libc::STDIN_FILENO, &mut termios as *mut libc::termios);
+
+        let errno = libc::tcgetattr(libc::STDIN_FILENO, &mut termios as *mut libc::termios);
+        if errno == -1 { panic!("tcgetattr") }
+
         ORIG_TERMIOS = Some(termios);
         libc::atexit(disable_raw_mode);
+
         termios.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
         termios.c_oflag &= !libc::OPOST;
         termios.c_cflag |= libc::CS8;
         termios.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
         termios.c_cc[libc::VMIN] = 0;
         termios.c_cc[libc::VTIME] = 1;
-        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+
+        let errno = libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+        if errno == -1 { panic!("tcsetattr") }
     }
 }
 
