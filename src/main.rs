7382ab392b7b2d7c2889628e4e4795c1711684ce
Nathan Bouscal <nbouscal@gmail.com>
2017-04-06 21:36:27 -0700

    Disable raw mode at exit
    In zsh this isn't an issue, but in bash without this ECHO remains off
    after the program exits.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@ extern crate libc;
 
 use std::io;
 
+static mut ORIG_TERMIOS: Option<libc::termios> = None;
+
+extern "C" fn disable_raw_mode() {
+    unsafe {
+        let mut termios = ORIG_TERMIOS.unwrap();
+        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+    }
+}
+
 fn enable_raw_mode() {
     unsafe {
         let mut termios: libc::termios = std::mem::zeroed();
         libc::tcgetattr(libc::STDIN_FILENO, &mut termios as *mut libc::termios);
+        ORIG_TERMIOS = Some(termios);
+        libc::atexit(disable_raw_mode);
         termios.c_lflag &= !libc::ECHO;
         libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
     }
