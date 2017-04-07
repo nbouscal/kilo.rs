edceefa1195197980ed5e99cfd6022cd9b0f30e0
Nathan Bouscal <nbouscal@gmail.com>
2017-04-07 01:01:17 -0700

    Pull out a terminal module

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,12 @@
 extern crate libc;
 
+mod terminal;
+
 use std::io::{self, Read, Write};
 use std::process;
 
-static mut ORIG_TERMIOS: Option<libc::termios> = None;
 
 fn ctrl_key(key: u8) -> u8 { key & 0x1f }
-
-extern "C" fn disable_raw_mode() {
-    unsafe {
-        let mut termios = ORIG_TERMIOS.unwrap();
-        let errno = libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
-        if errno == -1 { panic!("tcsetattr") }
-    }
-}
-
-fn enable_raw_mode() {
-    unsafe {
-        let mut termios: libc::termios = std::mem::zeroed();
-
-        let errno = libc::tcgetattr(libc::STDIN_FILENO, &mut termios as *mut libc::termios);
-        if errno == -1 { panic!("tcgetattr") }
-
-        ORIG_TERMIOS = Some(termios);
-        libc::atexit(disable_raw_mode);
-
-        termios.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
-        termios.c_oflag &= !libc::OPOST;
-        termios.c_cflag |= libc::CS8;
-        termios.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
-        termios.c_cc[libc::VMIN] = 0;
-        termios.c_cc[libc::VTIME] = 1;
-
-        let errno = libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
-        if errno == -1 { panic!("tcsetattr") }
-    }
-}
 
 fn editor_read_key() -> u8 {
     let mut c = [0; 1];
@@ -68,7 +39,7 @@ fn editor_process_keypress() {
 }
 
 fn main() {
-    enable_raw_mode();
+    terminal::enable_raw_mode();
     loop {
         editor_refresh_screen();
         editor_process_keypress();
---
+++ src/terminal.rs
@@ -0,0 +1,35 @@
+use libc;
+
+use std::mem;
+
+static mut ORIG_TERMIOS: Option<libc::termios> = None;
+
+extern "C" fn disable_raw_mode() {
+    unsafe {
+        let mut termios = ORIG_TERMIOS.unwrap();
+        let errno = libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+        if errno == -1 { panic!("tcsetattr") }
+    }
+}
+
+pub fn enable_raw_mode() {
+    unsafe {
+        let mut termios: libc::termios = mem::zeroed();
+
+        let errno = libc::tcgetattr(libc::STDIN_FILENO, &mut termios as *mut libc::termios);
+        if errno == -1 { panic!("tcgetattr") }
+
+        ORIG_TERMIOS = Some(termios);
+        libc::atexit(disable_raw_mode);
+
+        termios.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
+        termios.c_oflag &= !libc::OPOST;
+        termios.c_cflag |= libc::CS8;
+        termios.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
+        termios.c_cc[libc::VMIN] = 0;
+        termios.c_cc[libc::VTIME] = 1;
+
+        let errno = libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+        if errno == -1 { panic!("tcsetattr") }
+    }
+}
