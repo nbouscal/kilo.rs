e7772e69bbddf29fa064e36d34d3e39ca4f2d09c
Nathan Bouscal <nbouscal@gmail.com>
2017-04-06 21:11:47 -0700

    Start enabling raw mode by disabling ECHO

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+extern crate libc;
+
 use std::io::{self, Read};
 
+fn enable_raw_mode() {
+    unsafe {
+        let mut termios: libc::termios = std::mem::zeroed();
+        libc::tcgetattr(libc::STDIN_FILENO, &mut termios as *mut libc::termios);
+        termios.c_lflag &= !libc::ECHO;
+        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &mut termios);
+    }
+}
+
 fn main() {
+    enable_raw_mode();
+
     let mut buffer = String::new();
     let _ = io::stdin().read_to_string(&mut buffer);
 
